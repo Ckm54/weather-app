2205f49a8847ea2634732074df3936937877e926
collo <mutaic84@gmail.com>
2022-12-01 09:33:04 +0300

    :hammer: update location file to handle (De)serialization

--- packages/open_meteo_api/lib/src/models/location.dart
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -8,13 +8,20 @@
 //         "longitude": -87.65005
 //     ]
 // }
+import 'package:json_annotation/json_annotation.dart';
 
+part 'location.g.dart';
+
+@JsonSerializable()
 class Locaton {
   const Locaton(
       {required this.id,
       required this.name,
       required this.latitude,
       required this.longitude});
+
+  factory Locaton.fromJson(Map<string, dynamic> json) =>
+      _$LocationFromJson(json);
 
   final int id;
   final String name;
